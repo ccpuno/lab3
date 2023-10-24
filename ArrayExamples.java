266070f5b1b9c46c742f1d0beb54209d468c32cb
ccpuno <148397973+ccpuno@users.noreply.github.com>
2023-10-24 11:02:57 -0700

    Update ArrayExamples.java & ArrayTests.java

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -1,24 +1,41 @@
 
+//Charlize Puno
 
 public class ArrayExamples {
 
-  // Changes the input array to be in reversed order
-  static void reverseInPlace(int[] arr) {
-    for(int i = 0; i < arr.length; i += 1) {
+  // {1,2,3,4,5} Changes the input array to be in reversed order
+  // static void reverseInPlace(int[] arr) {
+  //   for(int i = 0; i < arr.length; i += 1) {
+  //     arr[i] = arr[arr.length - i - 1];
+  //   }
+  // }
+  static void reverseInPlace(int[] arr){
+    for(int i = 0; i < arr.length/2; i++){
+      int revArr = arr[i]; 
       arr[i] = arr[arr.length - i - 1];
+      arr[arr.length - i - 1] = revArr;
+      }
     }
-  }
+   
 
   // Returns a *new* array with all the elements of the input array in reversed
-  // order
-  static int[] reversed(int[] arr) {
-    int[] newArray = new int[arr.length];
-    for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
-    }
-    return arr;
-  }
+  // order 
+  // static int[] reversed(int[] arr) {
+  //   int[] newArray = new int[arr.length];
+  //   for(int i = 0; i < arr.length; i += 1) {
+  //     arr[i] = newArray[arr.length - i - 1];
+  //   }
+  //   return arr;
+  // }
 
+  static int[] reversed(int[] arr){
+    int[] newArray = new int[arr.length];
+    for(int i = 0; i < arr.length; i++){
+      newArray[arr.length - i- 1] = arr[i];
+    }
+    return newArray;
+  }
+  
   // Averages the numbers in the array (takes the mean), but leaves out the
   // lowest number when calculating. Returns 0 if there are no elements or just
   // 1 element in the array
